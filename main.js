39463f91e630fa2442717faddd4a05c503a218ab
MicheleUsai91 <Usai91@hotmail.com>
2023-07-09 10:13:17 +0200

    Responsive grid

--- main.js
+++ main.js
@@ -1,152 +1,151 @@
 const BOOKMARKS = {
     "books": [
-        { "name": "Libgen", "link": "https://libgen.is" },
-        { "name": "Sci-Hub", "link": "https://sci-hub.41610.org" },
-        { "name": "Z-Library", "link": "https://z-lib.org" }
+        { "name": "libgen", "link": "https://libgen.is" },
+        { "name": "sci-hub", "link": "https://sci-hub.41610.org" },
+        { "name": "z-library", "link": "https://z-lib.org" }
     ],
     "cloud": [
-        { "name": "iCloud", "link": "https://beta.icloud.com" },
-        { "name": "GDrive", "link": "https://drive.google.com/drive/u/0/my-drive" },
-        { "name": "OneDrive", "link": "https://onedrive.live.com" }
+        { "name": "icloud", "link": "https://beta.icloud.com" },
+        { "name": "gdrive", "link": "https://drive.google.com/drive/u/0/my-drive" },
+        { "name": "onedrive", "link": "https://onedrive.live.com" }
     ],
     "manga": [
-        { "name": "Bakemonogatari", "link": "https://mangakatana.com/manga/bakemonogatari-nishio-ishin.20047" },
-        // {"name" : "Baki-Dou (2018)", "link" : "https://mangakatana.com/manga/baki-dou-2018.21422"},
-        { "name": "Berserk", "link": "https://mangakatana.com/manga/berserk.1087" },
-        { "name": "Boku no Hero Academia", "link": "https://mangakatana.com/manga/boku-no-hero-academia.551" },
-        { "name": "Chainsaw Man", "link": "https://mangakatana.com/manga/chainsaw-man.21890" },
-        { "name": "Guard Pass", "link": "https://mangakatana.com/manga/guard-pass.25929" },
-        { "name": "Juujika no Rokunin", "link": "https://mangakatana.com/manga/juujika-no-rokunin.25073" },
-        { "name": "Kengan Omega", "link": "https://mangakatana.com/manga/kengan-omega.22093" },
-        { "name": "Made in Abyss", "link": "https://mangakatana.com/manga/made-in-abyss.1224" },
-        { "name": "Nagatoro", "link": "https://mangakatana.com/manga/please-dont-bully-me-nagatoro.19551" },
-        { "name": "Onepunch-Man", "link": "https://mangakatana.com/manga/onepunch-man.437" },
-        { "name": "Sakamoto Days", "link": "https://mangakatana.com/manga/sakamoto-days.25740" }
+        { "name": "bakemonogatari", "link": "https://mangakatana.com/manga/bakemonogatari-nishio-ishin.20047" },
+        // {"name" : "baki-dou (2018)", "link" : "https://mangakatana.com/manga/baki-dou-2018.21422"},
+        { "name": "berserk", "link": "https://mangakatana.com/manga/berserk.1087" },
+        { "name": "boku no hero academia", "link": "https://mangakatana.com/manga/boku-no-hero-academia.551" },
+        { "name": "chainsaw man", "link": "https://mangakatana.com/manga/chainsaw-man.21890" },
+        { "name": "guard pass", "link": "https://mangakatana.com/manga/guard-pass.25929" },
+        { "name": "juujika no rokunin", "link": "https://mangakatana.com/manga/juujika-no-rokunin.25073" },
+        { "name": "kengan omega", "link": "https://mangakatana.com/manga/kengan-omega.22093" },
+        { "name": "made in abyss", "link": "https://mangakatana.com/manga/made-in-abyss.1224" },
+        { "name": "nagatoro", "link": "https://mangakatana.com/manga/please-dont-bully-me-nagatoro.19551" },
+        { "name": "onepunch-man", "link": "https://mangakatana.com/manga/onepunch-man.437" },
+        { "name": "sakamoto days", "link": "https://mangakatana.com/manga/sakamoto-days.25740" }
     ],
     "p2p": [
-        { "name": "FitGirl", "link": "https://www.1377x.to/user/FitGirl" },
-        { "name": "NAHOMHD", "link": "https://www.1377x.to/user/NAHOMhd" }
+        { "name": "fitgirl", "link": "https://www.1377x.to/user/fitgirl" },
+        { "name": "nahomhd", "link": "https://www.1377x.to/user/nahomhd" }
     ],
     "poste": [
-        { "name": "PEC Webmail", "link": "https://webmail.postecert.it/certmail/index-rui.jsp?v=3.0.86.0#" },
-        { "name": "Poste Italiane", "link": "https://www.poste.it" }
+        { "name": "pec webmail", "link": "https://webmail.postecert.it/certmail/index-rui.jsp?v=3.0.86.0#" },
+        { "name": "poste italiane", "link": "https://www.poste.it" }
     ],
     "personale": [
-        { "name": "ACI", "link": "https://www.aci.it" },
-        { "name": "Agenzia delle Entrate", "link": "https://www.agenziaentrate.gov.it/portale/home" },
-        { "name": "Comune di Cagliari", "link": "https://www.comune.cagliari.it/portale" },
-        { "name": "Il Portale dell'Automobilista", "link": "https://www.ilportaledellautomobilista.it/web/portale-automobilista/home" },
-        { "name": "MyAnpal", "link": "https://myanpal.anpal.gov.it" },
-        { "name": "Sardegna Lavoro", "link": "https://www.sardegnalavoro.it" }
+        { "name": "aci", "link": "https://www.aci.it" },
+        { "name": "agenzia delle entrate", "link": "https://www.agenziaentrate.gov.it/portale/home" },
+        { "name": "comune di cagliari", "link": "https://www.comune.cagliari.it/portale" },
+        { "name": "il portale dell'automobilista", "link": "https://www.ilportaledellautomobilista.it/web/portale-automobilista/home" },
+        { "name": "myanpal", "link": "https://myanpal.anpal.gov.it" },
+        { "name": "sardegna lavoro", "link": "https://www.sardegnalavoro.it" }
     ],
     "resources": [
-        { "name": "Advanced Bash-Scripting Guide", "link": "https://tldp.org/LDP/abs/html" },
-        { "name": "Apple UI Design", "link": "https://developer.apple.com/design/human-interface-guidelines" },
-
-        { "name": "DevDocs", "link": "https://devdocs.io" },
-        { "name": "FullStack", "link": "https://fullstackopen.com/en" },
-        { "name": "Must Read Books", "link": "https://www.reddit.com/r/learnprogramming/comments/83xyow/must_read_books" },
-        { "name": "Palladium Magazin", "link": "https://www.palladiummag.com" },
-        { "name": "Roadmap WebDev", "link": "https://github.com/kamranahmedse/developer-roadmap" },
-        { "name": "PHP The Right Way", "link": "https://phptherightway.com" }
+        { "name": "advanced bash-scripting guide", "link": "https://tldp.org/ldp/abs/html" },
+        { "name": "apple ui design", "link": "https://developer.apple.com/design/human-interface-guidelines" },
+        { "name": "devdocs", "link": "https://devdocs.io" },
+        { "name": "fullstack", "link": "https://fullstackopen.com/en" },
+        { "name": "must read books", "link": "https://www.reddit.com/r/learnprogramming/comments/83xyow/must_read_books" },
+        { "name": "palladium magazine", "link": "https://www.palladiummag.com" },
+        { "name": "roadmap webdev", "link": "https://github.com/kamranahmedse/developer-roadmap" },
+        { "name": "php the right way", "link": "https://phptherightway.com" }
     ],
     "shopping": [
-        { "name": "AliExpress", "link": "http://it.aliexpress.com" },
-        { "name": "Amazon", "link": "http://www.amazon.it" },
-        { "name": "ePrice", "link": "http://www.eprice.it" },
-        { "name": "Mediaworld", "link": "http://www.mediaworld.it" },
-        { "name": "Subito", "link": "http://www.subito.it" }
+        { "name": "aliexpress", "link": "http://it.aliexpress.com" },
+        { "name": "amazon", "link": "http://www.amazon.it" },
+        { "name": "eprice", "link": "http://www.eprice.it" },
+        { "name": "mediaworld", "link": "http://www.mediaworld.it" },
+        { "name": "subito", "link": "http://www.subito.it" }
     ],
     "socials": [
-        { "name": "LinkedIn", "link": "https://www.linkedin.com/feed" },
-        { "name": "Telegram", "link": "https://web.telegram.org/#/login" },
-        { "name": "Whatsapp", "link": "https://web.whatsapp.com" }
+        { "name": "linkedin", "link": "https://www.linkedin.com/feed" },
+        { "name": "telegram", "link": "https://web.telegram.org/#/login" },
+        { "name": "whatsapp", "link": "https://web.whatsapp.com" }
     ],
     "svago": [
-        { "name": "Nexus Mods", "link": "https://www.nexusmods.com" },
-        { "name": "Spotify", "link": "https://open.spotify.com" }
+        { "name": "nexus mods", "link": "https://www.nexusmods.com" },
+        { "name": "spotify", "link": "https://open.spotify.com" }
     ],
     "tools": [
-        { "name": "Chat-GPT", "link": "https://chat.openai.com" },
-        // {"name" : "Clean Installation", "link" : "C:/Users/Michele/Documents/Programmazione/Code/WebApp/CleanInstall/CleanInstall.html"},
-        { "name": "ExRx", "link": "https://exrx.net" },
-        { "name": "GitHub", "link": "https://github.com" },
-        { "name": "HexEd", "link": "https://hexed.it" },
-        { "name": "Overleaf", "link": "https://it.overleaf.com" },
-        { "name": "Phind", "link": "https://www.phind.com" },
-        { "name": "PHPInfo", "link": "http://localhost:4000/Documents/Programmazione/Code/WebApp/Startpage/Pages/PHPInfo.php" },
-        { "name": "SpeedTest", "link": "https://librespeed.org" },
-        { "name": "Tuxbell", "link": "http://tuxbell.com" },
-        { "name": "Wiby", "link": "https://wiby.me" },
-        { "name": "Wolfram|Alpha", "link": "https://www.wolframalpha.com" }
+        { "name": "chat-gpt", "link": "https://chat.openai.com" },
+        // {"name" : "clean installation", "link" : "c:/users/michele/documents/programmazione/code/webapp/cleaninstall/cleaninstall.html"},
+        { "name": "exrx", "link": "https://exrx.net" },
+        { "name": "github", "link": "https://github.com" },
+        { "name": "hexed", "link": "https://hexed.it" },
+        { "name": "overleaf", "link": "https://it.overleaf.com" },
+        { "name": "phind", "link": "https://www.phind.com" },
+        { "name": "phpinfo", "link": "http://localhost:4000/documents/programmazione/code/webapp/startpage/pages/phpinfo.php" },
+        { "name": "speedtest", "link": "https://librespeed.org" },
+        { "name": "tuxbell", "link": "http://tuxbell.com" },
+        { "name": "wiby", "link": "https://wiby.me" },
+        { "name": "wolfram|alpha", "link": "https://www.wolframalpha.com" }
     ],
     "utility": [
-        { "name": "Modem", "link": "http://192.168.1.1" },
-        { "name": "PDFescape", "link": "http://www.pdfescape.com/open" },
-        { "name": "Router", "link": "http://192.168.0.1/webpages/index.1476264749915.html" },
-        { "name": "SketchUp", "link": "https://app.sketchup.com/app" }
+        { "name": "modem", "link": "http://192.168.1.1" },
+        { "name": "pdfescape", "link": "http://www.pdfescape.com/open" },
+        { "name": "router", "link": "http://192.168.0.1/webpages/index.1476264749915.html" },
+        { "name": "sketchup", "link": "https://app.sketchup.com/app" }
     ],
     "work": [
-        { "name": "Box", "link": "https://app.box.com/folder/0" },
-        { "name": "Chime", "link": "https://app.chime.aws" },
-        { "name": "Fondo EST", "link": "https://my.fondoest.it" },
-        { "name": "GestioneVPN", "link": "https://app.box.com/s/2x6910n37g1ftl4xasq70lbowcevr6m" },
-        { "name": "Odoo", "link": "https://pccube.odoo.com/web#cids=1&home=" },
-        { "name": "Sconti Aziendali", "link": "https://pccube.convenzioniaziendali.it" },
-        { "name": "Upwork", "link": "https://www.upwork.com" },
-        { "name": "Webmail", "link": "https://webmailbeta.aruba.it" }
+        { "name": "box", "link": "https://app.box.com/folder/0" },
+        { "name": "chime", "link": "https://app.chime.aws" },
+        { "name": "fondo est", "link": "https://my.fondoest.it" },
+        { "name": "gestionevpn", "link": "https://app.box.com/s/2x6910n37g1ftl4xasq70lbowcevr6m" },
+        { "name": "odoo", "link": "https://pccube.odoo.com/web#cids=1&home=" },
+        { "name": "sconti aziendali", "link": "https://pccube.convenzioniaziendali.it" },
+        { "name": "upwork", "link": "https://www.upwork.com" },
+        { "name": "webmail", "link": "https://webmailbeta.aruba.it" }
     ],
     "youtube": [
-        { "name": "Youtube", "link": "https://www.youtube.com" },
-        { "name": "Dictionary of Obscure Sorrows", "link": "https://www.youtube.com/c/obscuresorrows" },
-        { "name": "DIY Perks", "link": "https://www.youtube.com/@DIYPerks" },
-        { "name": "Isaac Arthur", "link": "https://www.youtube.com/@isaacarthur3209" },
-        { "name": "Jack PGM", "link": "https://www.youtube.com/@JackPGM" },
-        { "name": "JerryRigEverything", "link": "https://www.youtube.com/@JerryRigEverything" },
-        { "name": "Kiwami Japan", "link": "https://www.youtube.com/channel/UCg3qsVzHeUt5_cPpcRtoaJQ" },
-        { "name": "Kurzgesagt", "link": "https://www.youtube.com/@kurzgesagt" },
-        { "name": "Linus Tech Tips", "link": "https://www.youtube.com/@LinusTechTips" },
-        { "name": "MelodySheep", "link": "https://www.youtube.com/@melodysheep" },
-        { "name": "PewDiePie", "link": "https://www.youtube.com/user/PewDiePie" },
-        { "name": "ShortCircuit", "link": "https://www.youtube.com/@ShortCircuit" },
-        { "name": "Steve1989MREInfo", "link": "https://www.youtube.com/channel/UC2I6Et1JkidnnbWgJFiMeHA" },
-        { "name": "Stuff Made Here", "link": "https://www.youtube.com/@StuffMadeHere" },
-        { "name": "Techquickie", "link": "https://www.youtube.com/@Techquickie" },
-        { "name": "Veritasium", "link": "https://www.youtube.com/@veritasium" }
+        { "name": "youtube", "link": "https://www.youtube.com" },
+        { "name": "dictionary of obscure sorrows", "link": "https://www.youtube.com/c/obscuresorrows" },
+        { "name": "diy perks", "link": "https://www.youtube.com/@diyperks" },
+        { "name": "isaac arthur", "link": "https://www.youtube.com/@isaacarthur3209" },
+        { "name": "jack pgm", "link": "https://www.youtube.com/@jackpgm" },
+        { "name": "jerryrigeverything", "link": "https://www.youtube.com/@jerryrigeverything" },
+        { "name": "kiwami japan", "link": "https://www.youtube.com/channel/ucg3qsvzheut5_cppcrtoajq" },
+        { "name": "kurzgesagt", "link": "https://www.youtube.com/@kurzgesagt" },
+        { "name": "linus tech tips", "link": "https://www.youtube.com/@linustechtips" },
+        { "name": "melodysheep", "link": "https://www.youtube.com/@melodysheep" },
+        { "name": "pewdiepie", "link": "https://www.youtube.com/user/pewdiepie" },
+        { "name": "shortcircuit", "link": "https://www.youtube.com/@shortcircuit" },
+        { "name": "steve1989mreinfo", "link": "https://www.youtube.com/channel/uc2i6et1jkidnnbwgjfimeha" },
+        { "name": "stuff made here", "link": "https://www.youtube.com/@stuffmadehere" },
+        { "name": "techquickie", "link": "https://www.youtube.com/@techquickie" },
+        { "name": "veritasium", "link": "https://www.youtube.com/@veritasium" }
     ]
 }
 
 const REMINDERS = {
     "carte": [
-        { "desc": "Banco Posta", "data": "2025-08-01" },
-        { "desc": "Carta D'Identità", "data": "2032-07-15" },
-        { "desc": "Passaporto", "data": "2033-04-06" },
-        { "desc": "Patente", "data": "2023-07-15" },
-        { "desc": "Poste Pay", "data": "2026-05-01" },
-        { "desc": "Tessera Sanitaria", "data": "2028-01-12" }
+        { "desc": "banco posta", "data": "2025-08-01" },
+        { "desc": "carta d'identità", "data": "2032-07-15" },
+        { "desc": "passaporto", "data": "2033-04-06" },
+        { "desc": "patente", "data": "2023-07-15" },
+        { "desc": "poste pay", "data": "2026-05-01" },
+        { "desc": "tessera sanitaria", "data": "2028-01-12" }
     ],
     "compleanni": [
-        { "desc": "Anna Rita Sanna", "data": "1959-03-12" },
-        { "desc": "Antonella Sanna", "data": "1956-04-18" },
-        { "desc": "Elisa Raga", "data": "2000-08-31" },
-        { "desc": "Luca Lecis", "data": "1991-02-13" },
-        { "desc": "Michele Usai", "data": "1991-07-15" }
+        { "desc": "anna rita sanna", "data": "1959-03-12" },
+        { "desc": "antonella sanna", "data": "1956-04-18" },
+        { "desc": "elisa raga", "data": "2000-08-31" },
+        { "desc": "luca lecis", "data": "1991-02-13" },
+        { "desc": "michele usai", "data": "1991-07-15" }
     ],
     "fisco": [
         { "desc": "730", "data": "2024-06-16" }
     ],
     "macchina": [
-        { "desc": "Assicurazione", "data": "2023-07-13" },
-        { "desc": "Bollo", "data": "2023-12-01" },
-        { "desc": "Revisione", "data": "2025-05-01" }
+        { "desc": "assicurazione", "data": "2023-07-13" },
+        { "desc": "bollo", "data": "2023-12-01" },
+        { "desc": "revisione", "data": "2025-05-01" }
     ],
     "poste": [
-        { "desc": "Firma", "data": "2025-06-17" },
-        { "desc": "PEC", "data": "2024-01-05" },
-        { "desc": "SPID", "data": "2023-06-30" }
+        { "desc": "firma", "data": "2025-06-17" },
+        { "desc": "pec", "data": "2024-01-05" },
+        { "desc": "spid", "data": "2023-06-30" }
     ],
     "salute": [
-        { "desc": "Certificato Medico", "data": "2024-07-13" }
+        { "desc": "certificato medico", "data": "2024-07-13" }
     ],
 }
 
@@ -155,7 +154,12 @@ function openAllPagesByName(name) {
 }
 
 function openLinks() {
-    const urls = ['https://web.whatsapp.com/', 'https://webmailbeta.aruba.it/', 'https://app.chime.aws/'];
+    const urls = [
+        'https://web.whatsapp.com/', 
+        'https://webmailbeta.aruba.it/', 
+        'https://app.chime.aws/', 
+        'file:///C:/Users/Michele/Documents/Programmazione/Code/Public/bookmarks.github.io/index.html'
+    ];
     openUrls(urls);
 }
 
@@ -189,14 +193,17 @@ function dateDiff(date) {
 
 function showBookmarks() {
     const bookmarks = document.getElementById("bookmarks_list");
-
+    const gridDiv = document.createElement("div");
+    gridDiv.setAttribute("class", "grid");
     Object.keys(BOOKMARKS).forEach(bookmark => {
-        const bookmarkList = document.createElement("ul");
+        const bookmarkDiv = document.createElement("div");
+        bookmarkDiv.setAttribute("class", "cell");
         const bookmarkTitle = document.createElement("h2");
+        const bookmarkList = document.createElement("ul");
 
         bookmarkTitle.textContent = bookmark;
         bookmarkTitle.setAttribute("onclick", `openAllPagesByName('${bookmark.toUpperCase()}')`);
-        bookmarkList.appendChild(bookmarkTitle);
+        bookmarkDiv.appendChild(bookmarkTitle);
 
         BOOKMARKS[bookmark].forEach(b => {
             const bookmarkItem = document.createElement("li");
@@ -209,15 +216,16 @@ function showBookmarks() {
 
             bookmarkItem.appendChild(bookmarkLink);
             bookmarkList.appendChild(bookmarkItem);
+            bookmarkDiv.appendChild(bookmarkList);
         });
-        bookmarks.appendChild(bookmarkList);
+        gridDiv.appendChild(bookmarkDiv);
+        bookmarks.appendChild(gridDiv);
     });
 }
 
 function showReminders() {
     const col = ["DESC", "DATA", "Y-M-D"];
     const reminders = document.getElementById("reminders_list");
-    const reminderList = document.createElement("ul");
     const table = document.createElement("table");
     const head = document.createElement("thead");
     const mainHeadRow = document.createElement("tr");
@@ -255,8 +263,7 @@ function showReminders() {
 
         table.appendChild(body);
     });
-    reminderList.appendChild(table);
-    reminders.appendChild(reminderList);
+    reminders.appendChild(table);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
