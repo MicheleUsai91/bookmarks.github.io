427da762ff7fee0ac41ee1a8e1dd8a688070cbef
MicheleUsai91 <Usai91@hotmail.com>
2023-10-24 16:23:16 +0200

    Revision + Workout

--- main.js
+++ main.js
@@ -1,200 +1,3 @@
-const BOOKMARKS = {
-    "books": [
-        { "name": "libgen", "link": "https://libgen.is" },
-        { "name": "sci-hub", "link": "https://sci-hub.41610.org" },
-        { "name": "z-library", "link": "https://z-lib.org" }
-    ],
-    "cloud": [
-        { "name": "icloud", "link": "https://beta.icloud.com" },
-        { "name": "gdrive", "link": "https://drive.google.com/drive/u/0/my-drive" },
-        { "name": "onedrive", "link": "https://onedrive.live.com" }
-    ],
-    "manga": [
-        { "name": "a story about a grandpa & granma", "link": "https://mangakatana.com/manga/a-story-about-a-grandpa-and-grandma-who-returned-back-to-their-youth.23710" },
-        { "name": "bakemonogatari", "link": "https://mangakatana.com/manga/bakemonogatari-nishio-ishin.20047" },
-        { "name": "baki rahen", "link": "https://mangakatana.com/manga/baki-rahen.27070" },
-        { "name": "berserk", "link": "https://mangakatana.com/manga/berserk.1087" },
-        { "name": "boku no hero academia", "link": "https://mangakatana.com/manga/boku-no-hero-academia.551" },
-        { "name": "chainsaw man", "link": "https://mangakatana.com/manga/chainsaw-man.21890" },
-        { "name": "dai dark", "link": "https://mangakatana.com/manga/dai-dark.22430" },
-        { "name": "guard pass", "link": "https://mangakatana.com/manga/guard-pass.25929" },
-        { "name": "hundred ghost stories", "link": "https://mangakatana.com/manga/hundred-ghost-stories-of-my-own-death.26032" },
-        { "name": "juujika no rokunin", "link": "https://mangakatana.com/manga/juujika-no-rokunin.25073" },
-        { "name": "kengan omega", "link": "https://mangakatana.com/manga/kengan-omega.22093" },
-        { "name": "made in abyss", "link": "https://mangakatana.com/manga/made-in-abyss.1224" },
-        { "name": "nagatoro", "link": "https://mangakatana.com/manga/please-dont-bully-me-nagatoro.19551" },
-        { "name": "onepunch-man", "link": "https://mangakatana.com/manga/onepunch-man.437" },
-        { "name": "sakamoto days", "link": "https://mangakatana.com/manga/sakamoto-days.25740" },
-        { "name": "senpai ga urusai kouhai no hanashi", "link": "https://mangakatana.com/manga/senpai-ga-urusai-kouhai-no-hanashi.20036" },
-        { "name": "under ninja", "link": "https://mangakatana.com/manga/under-ninja.21115" },
-        { "name": "yajin tensei", "link": "https://mangakatana.com/manga/yajin-tensei-karate-survivor-in-another-world.24947" }
-    ],
-    "p2p": [
-        { "name": "fitgirl", "link": "https://www.1377x.to/user/fitgirl" },
-        { "name": "nahomhd", "link": "https://www.1377x.to/user/nahomhd" }
-    ],
-    "poste": [
-        { "name": "pec webmail", "link": "https://webmail.postecert.it/certmail/index-rui.jsp?v=3.0.86.0#" },
-        { "name": "poste italiane", "link": "https://www.poste.it" }
-    ],
-    "personale": [
-        { "name": "aci", "link": "https://www.aci.it" },
-        { "name": "agenzia delle entrate", "link": "https://www.agenziaentrate.gov.it/portale/home" },
-        { "name": "comune di cagliari", "link": "https://www.comune.cagliari.it/portale" },
-        { "name": "il portale dell'automobilista", "link": "https://www.ilportaledellautomobilista.it/web/portale-automobilista/home" },
-        { "name": "myanpal", "link": "https://myanpal.anpal.gov.it" },
-        { "name": "sardegna lavoro", "link": "https://www.sardegnalavoro.it" }
-    ],
-    "resources": [
-        { "name": "advanced bash-scripting guide", "link": "https://tldp.org/ldp/abs/html" },
-        { "name": "apple ui design", "link": "https://developer.apple.com/design/human-interface-guidelines" },
-        { "name": "devdocs", "link": "https://devdocs.io" },
-        { "name": "fullstack", "link": "https://fullstackopen.com/en" },
-        { "name": "must read books", "link": "https://www.reddit.com/r/learnprogramming/comments/83xyow/must_read_books" },
-        { "name": "roadmap webdev", "link": "https://github.com/kamranahmedse/developer-roadmap" },
-        { "name": "php the right way", "link": "https://phptherightway.com" }
-    ],
-    "shopping": [
-        { "name": "aliexpress", "link": "http://it.aliexpress.com" },
-        { "name": "amazon", "link": "http://www.amazon.it" },
-        { "name": "eprice", "link": "http://www.eprice.it" },
-        { "name": "mediaworld", "link": "http://www.mediaworld.it" },
-        { "name": "subito", "link": "http://www.subito.it" }
-    ],
-    "socials": [
-        { "name": "linkedin", "link": "https://www.linkedin.com/feed" },
-        { "name": "telegram", "link": "https://web.telegram.org/#/login" },
-        { "name": "whatsapp", "link": "https://web.whatsapp.com" }
-    ],
-    "svago": [
-        { "name": "nexus mods", "link": "https://www.nexusmods.com" },
-        { "name": "spotify", "link": "https://open.spotify.com" }
-    ],
-    "tools": [
-        { "name": "chat-gpt", "link": "https://chat.openai.com" },
-        { "name": "exrx", "link": "https://exrx.net" },
-        { "name": "github", "link": "https://github.com" },
-        { "name": "hexed", "link": "https://hexed.it" },
-        { "name": "overleaf", "link": "https://it.overleaf.com" },
-        { "name": "phind", "link": "https://www.phind.com" },
-        { "name": "phpinfo", "link": "http://localhost:4000/documents/programmazione/code/webapp/startpage/pages/phpinfo.php" },
-        { "name": "speedtest", "link": "https://librespeed.org" },
-        { "name": "tuxbell", "link": "http://tuxbell.com" },
-        { "name": "wiby", "link": "https://wiby.me" },
-        { "name": "wolfram|alpha", "link": "https://www.wolframalpha.com" }
-    ],
-    "utility": [
-        { "name": "modem", "link": "http://192.168.1.1" },
-        { "name": "pdfescape", "link": "http://www.pdfescape.com/open" },
-        { "name": "router", "link": "http://192.168.0.1/webpages/index.1476264749915.html" },
-        { "name": "sketchup", "link": "https://app.sketchup.com/app" }
-    ],
-    "work": [
-        { "name": "box", "link": "https://app.box.com/folder/0" },
-        { "name": "chime", "link": "https://app.chime.aws" },
-        { "name": "fondo est", "link": "https://my.fondoest.it" },
-        { "name": "gestionevpn", "link": "https://app.box.com/s/2x6910n37g1ftl4xasq70lbowcevr6m" },
-        { "name": "odoo", "link": "https://pccube.odoo.com/web#cids=1&home=" },
-        { "name": "sconti aziendali", "link": "https://pccube.convenzioniaziendali.it" },
-        { "name": "upwork", "link": "https://www.upwork.com" },
-        { "name": "webmail", "link": "https://webmailbeta.aruba.it" }
-    ],
-    "youtube": [
-        { "name": "youtube", "link": "https://www.youtube.com" },
-        { "name": "dictionary of obscure sorrows", "link": "https://www.youtube.com/c/obscuresorrows" },
-        { "name": "diy perks", "link": "https://www.youtube.com/@diyperks" },
-        { "name": "isaac arthur", "link": "https://www.youtube.com/@isaacarthurSFIA" },
-        { "name": "jack pgm", "link": "https://www.youtube.com/@jackpgm" },
-        { "name": "jerryrigeverything", "link": "https://www.youtube.com/@jerryrigeverything" },
-        { "name": "kiwami japan", "link": "https://www.youtube.com/channel/ucg3qsvzheut5_cppcrtoajq" },
-        { "name": "kurzgesagt", "link": "https://www.youtube.com/@kurzgesagt" },
-        { "name": "linus tech tips", "link": "https://www.youtube.com/@linustechtips" },
-        { "name": "melodysheep", "link": "https://www.youtube.com/@melodysheep" },
-        { "name": "pewdiepie", "link": "https://www.youtube.com/user/pewdiepie" },
-        { "name": "shortcircuit", "link": "https://www.youtube.com/@shortcircuit" },
-        { "name": "steve1989mreinfo", "link": "https://www.youtube.com/channel/uc2i6et1jkidnnbwgjfimeha" },
-        { "name": "stuff made here", "link": "https://www.youtube.com/@stuffmadehere" },
-        { "name": "techquickie", "link": "https://www.youtube.com/@techquickie" },
-        { "name": "veritasium", "link": "https://www.youtube.com/@veritasium" }
-    ]
-}
-
-const NEWS  = {
-    "economy": [
-        { "name": "bloomberg", "link": "https://www.bloomberg.com/europe" },
-        { "name": "market watch", "link": "https://www.marketwatch.com" },
-        { "name": "the economist", "link": "https://www.economist.com" }
-    ],
-    "magazine": [
-        { "name": "palladium magazine", "link": "https://www.palladiummag.com" },
-        { "name": "time", "link": "https://time.com" }
-        
-    ],
-    "sardegna": [
-        { "name": "ansa sardegna", "link": "https://www.ansa.it/sardegna" },
-        { "name": "casteddu online", "link": "https://www.castedduonline.it" },
-        { "name": "l'Unione sarda", "link": "https://www.unionesarda.it" },
-        { "name": "la nuova sardegna", "link": "https://www.lanuovasardegna.it" }
-    ],
-    "science": [
-        { "name" : "big think", "link" : "https://bigthink.com/"},
-        { "name": "live science", "link": "https://www.livescience.com" },
-        { "name": "new scientist", "link": "https://www.newscientist.com" },
-        { "name": "phys org", "link": "https://phys.org" },
-        { "name": "scientific american", "link": "https://www.scientificamerican.com" },
-        { "name": "science news", "link": "https://www.sciencenews.org" }
-    ],
-    "tech": [
-        { "name": "hacker news", "link": "https://news.ycombinator.com" },
-        { "name": "tech crunch", "link": "https://techcrunch.com" },
-        { "name": "wired", "link": "https://www.wired.com" }
-    ],
-    "world": [
-        { "name": "associated press", "link": "https://apnews.com" },
-        { "name": "reuters", "link": "https://www.reuters.com" },
-        { "name": "the guardian", "link": "https://www.theguardian.com/international" },
-        { "name": "the new york times", "link": "https://www.nytimes.com/international" },
-        { "name": "the times", "link": "https://www.thetimes.co.uk" },
-        { "name": "wall street journal", "link": "https://www.wsj.com" }
-    ]
-}
-
-const REMINDERS = {
-    "carte": [
-        { "desc": "banco posta", "data": "2025-08-01" },
-        { "desc": "carta d'identità", "data": "2032-07-15" },
-        { "desc": "ctm", "data": "2024-07-01" },
-        { "desc": "passaporto", "data": "2033-04-06" },
-        { "desc": "patente", "data": "2033-07-15" },
-        { "desc": "poste pay", "data": "2026-05-01" },
-        { "desc": "tessera sanitaria", "data": "2028-01-12" }
-    ],
-    "compleanni": [
-        { "desc": "anna rita sanna", "data": "1959-03-12" },
-        { "desc": "antonella sanna", "data": "1956-04-18" },
-        { "desc": "elisa raga", "data": "2000-08-31" },
-        { "desc": "luca lecis", "data": "1991-02-13" },
-        { "desc": "michele usai", "data": "1991-07-15" }
-    ],
-    "fisco": [
-        { "desc": "730", "data": "2024-06-16" }
-    ],
-    "macchina": [
-        { "desc": "assicurazione", "data": "2024-07-13" },
-        { "desc": "bollo", "data": "2023-12-01" },
-        { "desc": "revisione", "data": "2025-05-01" }
-    ],
-    "poste": [
-        { "desc": "firma", "data": "2025-06-17" },
-        { "desc": "pec", "data": "2024-01-05" }
-    ],
-    "salute": [
-        { "desc": "certificato medico", "data": "2024-07-12" },
-        { "desc": "palestra annuale", "data": "2024-07-13" },
-        { "desc": "palestra entrate", "data": "2023-09-13" }
-    ],
-}
-
 function openAllPagesByName(name) {
     openUrls(getLinksList(document.getElementsByName(name)));
 }
@@ -223,13 +26,6 @@ function getLinksList(array) {
     return linksList;
 }
 
-function showAccordion(value) {
-    const accordions = ["bookmarks_list", "reminders_list", "news_list"];
-    accordions.forEach(a => {
-        document.getElementById(a).hidden = a !== value;
-    })
-}
-
 function dateDiff(date) {
     const currentDate = new Date();
     const parsedDate = new Date(date);
@@ -240,87 +36,50 @@ function dateDiff(date) {
     ];
 }
 
-function showGrid(group, json) {
-    const section = document.getElementById(group);
-    const gridDiv = document.createElement("div");
-    gridDiv.setAttribute("class", "grid");
-    Object.keys(json).forEach(j => {
-        const sectionDiv = document.createElement("div");
-        sectionDiv.setAttribute("class", "cell");
-        const sectionTitle = document.createElement("h2");
-        const sectionList = document.createElement("ul");
-
-        sectionTitle.textContent = j;
-        sectionTitle.setAttribute("onclick", `openAllPagesByName('${j.toUpperCase()}')`);
-        sectionDiv.appendChild(sectionTitle);
-
-        json[j].forEach(a => {
-            const sectionItem = document.createElement("li");
-            const sectionLink = document.createElement("a");
-
-            sectionLink.href = a.link;
-            sectionLink.textContent = a.name;
-            sectionLink.target = "_blank";
-            sectionLink.setAttribute("name", j.toUpperCase());
-
-            sectionItem.appendChild(sectionLink);
-            sectionList.appendChild(sectionItem);
-            sectionDiv.appendChild(sectionList);
-        });
-        gridDiv.appendChild(sectionDiv);
-        section.appendChild(gridDiv);
-    });
-}
-
-function showReminders() {
-    const col = ["DESC", "DATA", "Y-M-D"];
-    const reminders = document.getElementById("reminders_list");
-    const table = document.createElement("table");
-    const head = document.createElement("thead");
-    const mainHeadRow = document.createElement("tr");
-    col.forEach(c => {
-        const header = document.createElement("th");
-        header.textContent = c;
-        mainHeadRow.appendChild(header);
+function calculateReminders() {
+    const dates = document.getElementsByName("date");
+    dates.forEach(d => {
+        let end = dateDiff(d.cells[1].innerHTML).map(value => Math.abs(value) < 10 ? "0" + Math.abs(value) : "" + Math.abs(value));
+        d.cells[2].innerHTML = `${end[0]} - ${end[1]} - ${end[2]}`;
+        if(end[0] + end[1] === "0000" && end[2] <= 7) {
+            d.cells[2].style.color = "red";
+            d.cells[2].style.fontWeight = "bold";
+        }
     })
-    head.appendChild(mainHeadRow);
-    table.appendChild(head);
-
-    Object.keys(REMINDERS).forEach(reminder => {
-        const subHead = document.createElement("thead");
-        const headRow = document.createElement("tr");
-        const header = document.createElement("th");
-        header.setAttribute("colspan", col.length);
-        header.textContent = reminder;
-        headRow.appendChild(header);
-        subHead.appendChild(headRow);
-        table.appendChild(subHead);
-        
-        const body = document.createElement("tbody");
-
-        REMINDERS[reminder].forEach(r => {
-            let data = dateDiff(r.data).map(value => Math.abs(value) < 10 ? "0" + Math.abs(value) : "" + Math.abs(value));
-            const bodyRow = document.createElement("tr");
-            col.forEach(c => {
-                const cell = document.createElement("td");
-                if(data[0] + data[1] === "0000" && data[2] <= 7) {
-                    cell.style.color = "red";
-                    cell.style.fontWeight = "bold";
-                }
-                cell.textContent = c !== "Y-M-D" ? r[c.toLowerCase()] : `${data[0]} - ${data[1]} - ${data[2]}`;
-                cell.setAttribute("data-cell", c);
-                bodyRow.appendChild(cell);
-            })
-            body.appendChild(bodyRow);
-        });
-
-        table.appendChild(body);
-    });
-    reminders.appendChild(table);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    showGrid("bookmarks_list", BOOKMARKS);
-    showReminders();
-    showGrid("news_list", NEWS);
+    if (window.location.href.includes("reminders")) calculateReminders();
 })
+
+function calcoloMassimale() {
+    let kg = document.getElementById("pesoMassimale").value;
+    let reps = document.getElementById("repsMassimale").value;
+    document.getElementById("resMassimale").innerHTML = Math.round(kg / (1.0278 - (0.0278 * reps)));
+}
+
+function calcoloReps() {
+    let max = document.getElementById("pesoReps").value;
+    let reps = document.getElementById("repsReps").value;
+    document.getElementById("resReps").innerHTML = Math.round(max * (1.0 - (reps / 40.0)));
+}
+
+function volume() {
+    let kg = document.getElementById("pesoVol").value;
+    let reps = document.getElementById("repsVol").value;
+    document.getElementById("resVol").innerHTML = Math.round(kg * reps);
+}
+
+function weightConverter() {
+    let weight = document.getElementById("pesoConv").value;
+    let type = document.querySelector('input[name="kgToLbs"]:checked').value;
+    let factor = type === "kg" ? 1.0 / 2.2046 : 2.2046;
+    document.getElementById("resConv").innerHTML = Math.round(weight * factor);
+}
+
+function timerStart() {
+    let work = document.getElementById("work").value;
+    let rest = document.getElementById("rest").value;
+    let sets = document.getElementById("sets").value;
+    console.log(work, rest, sets)
+}
